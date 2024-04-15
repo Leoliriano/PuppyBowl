c53183b25d9f3cf54219ff3829b89e4cfb03f523
Leoliriano <leoliriano@yahoo.com>
2024-04-15 17:14:37 -0500

    Final commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,19 @@
+
+export default function Home() {
+
+
+    return (
+        <>
+            <div className="home">
+                <h1>IT'S TIME FOR THE PUPPY BOWL!</h1>
+                <div className="homeBox">
+                    <h2>Feel free to..</h2>
+                    <h3>1. Check out all of our puppy superstars</h3> 
+                    <h3>2. Search a puppy</h3> 
+                    <h3>3. Sign up your own puppy to compete in the puppy bowl!</h3>
+                </div>
+            </div>
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+        </>
+    )
+}
